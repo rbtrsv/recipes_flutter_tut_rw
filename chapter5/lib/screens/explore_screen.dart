079a88015aa42928c9586e4038d53a58d79d8b29
Robert Radoslav <robert.radoslav@protonmail.ch>
2022-10-25 17:59:14 +0300

    Chapter5

---
+++ chapter5/lib/screens/explore_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import '../components/components.dart';
+import '../models/models.dart';
+import '../api/mock_fooderlich_service.dart';
+
+class ExploreScreen extends StatelessWidget {
+  // 1
+  final mockService = MockFooderlichService();
+  ExploreScreen({super.key});
+  @override
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: mockService.getExploreData(),
+      builder: (context, AsyncSnapshot<ExploreData> snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return ListView(
+            scrollDirection: Axis.vertical,
+            children: [
+              TodayRecipeListView(recipes: snapshot.data?.todayRecipes ?? []),
+
+              const SizedBox(height: 16),
+
+              // TODO: Replace this with FriendPostListView
+              FriendPostListView(friendPosts: snapshot.data?.friendPosts ?? []),
+            ],
+          );
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
+    );
+  }
+}
